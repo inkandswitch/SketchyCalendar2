5610fc2c84d95f24b973248f1e45aab64e8992fd
Paul Sonnentag <paul.sonnentag@googlemail.com>
2025-07-07 14:00:16 +0200

    build config.html and index.html

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,27 +2,16 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 import { defineConfig } from "vite";
-//import { EXTERNAL_DEPENDENCIES } from "@patchwork/sdk/shared-dependencies";
 
 export default defineConfig({
   base: "./",
   plugins: [tsconfigPaths(), wasm(), topLevelAwait()],
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       patchwork: "src/patchwork/index.ts",
-  //       index: "index.html",
-  //     },
-  //     output: {
-  //       entryFileNames: (chunkInfo) => {
-  //         if (chunkInfo.name === "patchwork") {
-  //           return "patchwork.js";
-  //         }
-
-  //         return "[name].[hash].js";
-  //       },
-  //     },
-  //     preserveEntrySignatures: "strict",
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        config: "./config.html",
+      },
+    },
+  },
 });
